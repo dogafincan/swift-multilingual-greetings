c1fb262d7cf5cd2266804313b93f5f6d94d5ed12
Doga Fincan <doga@icloud.com>
2022-09-06 23:24:46 +0200

    Use forced unwrapping

--- Sources/MultilingualGreetings/MultilingualGreetings.swift
+++ Sources/MultilingualGreetings/MultilingualGreetings.swift
@@ -2,16 +2,12 @@ import Gen
 
 var xoshiro = Xoshiro()
 
-public func randomMultilingualGreeting() -> String? {
+public func randomMultilingualGreeting() -> String {
     let greetings = ["안녕하세요", "こんにちは", "你好", "สวัสดีครับ", "Xin chào"]
     
     for greeting in greetings {
         assert(!greeting.isEmpty, "expected a non-empty string")
     }
     
-    guard let greeting = Gen.element(of: greetings).run(using: &xoshiro) else {
-        return nil
-    }
-    
-    return greeting
+    return Gen.element(of: greetings).run(using: &xoshiro)!
 }
